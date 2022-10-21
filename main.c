846f0e05b90b56d407015bd8bcf4bb90f0b97c1f
Jake <jakeroggenbuck2@gmail.com>
2022-10-21 16:56:11 -0700

    Add tests and gitignore

---
+++ main.c
@@ -0,0 +1,45 @@
+/* #include "C_string.h" */
+#include <stdio.h>
+#include <string.h>
+
+void output_test_strcmp(int c, char *name1, char *name2) {
+    if (c > 0) {
+        printf("%s is larger than %s.\n", name1, name2);
+    } else if (c < 0) {
+        printf("%s is smaller than %s.\n", name1, name2);
+    } else {
+        printf("%s and %s are equal.\n", name1, name2);
+    }
+    printf("num = %d\n\n", c);
+}
+
+int test_strcmp() {
+    char name1[5] = "Jake";
+    char name2[5] = "Adam";
+    int c = strcmp(name1, name2);
+    output_test_strcmp(c, name1, name2);
+
+    char name3[5] = "Adam";
+    char name4[5] = "Jake";
+    int d = strcmp(name3, name4);
+    output_test_strcmp(d, name3, name4);
+
+    char name5[5] = "Jake";
+    char name6[5] = "Jake";
+    int e = strcmp(name5, name6);
+    output_test_strcmp(e, name5, name6);
+
+    char name7[5] = "Jake";
+    char name8[7] = "Sophon";
+    int f = strcmp(name7, name8);
+    output_test_strcmp(f, name7, name8);
+
+    char name0[16] = "Rake Joggenbuck";
+    char name9[15] = "Hdam Autchings";
+    int g = strcmp(name9, name0);
+    output_test_strcmp(d, name9, name0);
+
+    printf("Adam is cooler tho.\n");
+}
+
+int main() { test_strcmp(); }
